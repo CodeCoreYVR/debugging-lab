CodeCoreYVR/awesome_answers_july_2016

add the question model with validation, scopes and callbacks

db/migrate/20160810165236_add_view_count_to_questions.rb
@@ -1,0 +1,9 @@
+class AddViewCountToQuestions < ActiveRecord::Migration[5.0]
+  def change
+    # add_column is a migration method that takes 3 arguements
+    # 1. Table name: questions
+    # 2. Column Name: view_count
+    # 3. Column Type: integer
+    add_column :questions, :view_count, :integer
+  end
+end

CodeCoreYVR/debugging-lab

db/migrate/20160810165236_add_view_count_to_questions.rb
@@ -1,0 +1,9 @@
+class AddViewCountToQuestions < ActiveRecord::Migration[5.0]
+  def change
+    # add_column is a migration method that takes 3 arguements
+    # 1. Table name: questions
+    # 2. Column Name: view_count
+    # 3. Column Type: integer
+    add_column :question, :view_count, :integer
+  end
+end
2e7353f11dcd679470596f7953558a08feea216d
Steve Godin <steve.god@gmail.com>
2016-10-12 11:07:19 -0700

    Seed answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-QUESTIONS_TO_CREATE = 500
+QUESTIONS_TO_CREATE = 250
+
+def create_answer
+  Answer.create(body: Faker::Pokemon.name)
+end
 
 QUESTIONS_TO_CREATE.times do
     Question.create title:      Faker::StarWars.quote,
                     body:       Faker::Hipster.paragraph,
                     view_count: rand(100)
+end
+
+Question.all.each do |question|
+  rand(2..4).times do
+    question.answers << create_answer
+  end
 end
 
 puts Cowsay.say "Created #{QUESTIONS_TO_CREATE} questions"

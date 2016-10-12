d217ac01204cdc96bed2111d79a78ab77b9a59a4
Steve Godin <steve.god@gmail.com>
2016-10-12 09:26:52 -0700

    Set Helvetica as base font

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -85,7 +85,7 @@ class QuestionsController < ApplicationController
 
     # we're using the `strong parameters` feature of Rails here to only allow
     # mass-assigning the attributes that we want to allow the user to set
-    params.require(:question).permit([:title, :body])
+    params.require(:question).permit([:titl, :body])
   end
 
 end
